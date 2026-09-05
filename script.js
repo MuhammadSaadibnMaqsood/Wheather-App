6c85f594cedfa47e50e02a6cf3cbb633ddbe3daf
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-09-05 10:51:45 -0700

    remove the api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "77bbf7f41fd03d8e145e8d78bdfe2d61";
+const API_KEY = "YOUR_API_KEY";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 async function fetchWheather() {
